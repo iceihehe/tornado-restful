23edabdeefa73a7d26e197b90093bf4d131a0b6f
iceihehe <iceihehe@gmail.com>
2016-12-14 15:44:25 +0800

    delete all invalid code

--- tornado_restful/handlers.py
+++ tornado_restful/handlers.py
@@ -8,22 +8,3 @@ class ApiHandler(tornado.web.RequestHandler):
     def initialize(self):
 
         self.set_header('Content-Type', 'application/json')
-
-    def fail(self, msg):
-
-        self.write({'msg': msg})
-        self.finish()
-
-    def write_error(self, status_code, **kwargs):
-
-        def get_exc_message(exception):
-
-            return exception.log_message if hasattr(exception, 'log_message') else str(exception)
-
-        exception = kwargs['exc_info'][1]
-
-        if status_code == 500:
-            self.fail('System error')
-
-        else:
-            self.fail(get_exc_message(exception))
